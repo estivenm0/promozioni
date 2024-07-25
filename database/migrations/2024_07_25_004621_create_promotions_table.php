3fccf1ff1df4b1b9739bcc4fa62c9258397215ee
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2024-07-24 21:33:03 -0500

    feat: 🎸 Add migrations

---
+++ database/migrations/2024_07_25_004621_create_promotions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('branch_id');
+            $table->unsignedTinyInteger('category_id');
+            $table->string('title', 50);
+            $table->string('slug')->unique();
+            $table->string('image');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
