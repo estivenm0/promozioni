791c4c9eb11bf8b77357aefc815aa41ec3dd93c4
stivenm0 <estivenyeizon@gmail.com>
2024-07-26 12:16:48 -0500

    feat: 🎸 add relationship to model and create seeders
    add relationship to models and create category and type seeders

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Branch extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'business_id',
+      'name',  
+      'status',  
+      'status_description',  
+      'address',  
+      'longitude',  
+      'latitude',  
+    ];
+
+    public function business() : BelongsTo
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    public function ratings() : HasMany
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function promotions() : HasMany
+    {
+        return $this->hasMany(Promotion::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+      'name'  
+    ];
+
+    public function promotions() : HasMany
+    {
+        return $this->hasMany(Promotion::class);
+    }
 }
